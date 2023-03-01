5fd1cae5f971cf8dd080bfcdc021b0871f52fc9c
Tiago Almeida <tiago.almeidat@outlook.com>
2023-03-01 14:29:55 -0300

    BtnClick ENABLE

--- classes/class01/pages/titles/src/factories/cardsFactory.js
+++ classes/class01/pages/titles/src/factories/cardsFactory.js
@@ -2,17 +2,16 @@ import CardsController from "./../controllers/cardsController.js"
 import CardsView from "./../views/cardsView.js"
 import CardsService from "./../services/cardsService.js"
 
-const camera = await Camera.init()
 const cardListWorker = new Worker(`./src/workers/cardListWorker.js`, { type: "module" })
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initalize() {
     return CardsController.initialize({
       view: new CardsView(),
-      service: new CardsService({ 
-      dbUrl: `${rootPath}/assets/database.json`,
-      cardListWorker
-    })
+      service: new CardsService({
+        dbUrl: `${rootPath}/assets/database.json`,
+        cardListWorker
+      })
     })
   }
 }
--- classes/class01/pages/video-player/src/factory.js
+++ classes/class01/pages/video-player/src/factory.js
@@ -2,15 +2,35 @@ import Camera from "../../../lib/shared/camera.js"
 import Controller from "./controller.js"
 import Service from "./service.js"
 import View from "./view.js"
+import { supportsWorkerType } from "../../../../lib/shared/util.js"
+
+async function getWorker() {
+
+    if (supportsWorkerType()) {
+        console.log('suporta!')
+        const worker = new Worker('./src/worker.js', { type: 'module' })
+        return worker
+    }
+
+    const workerMock = {
+        async postMessage() { },
+        onmessage(msg) { }
+    }
+
+    console.log('não suporta')
+    return workerMock
+}
+
+const worker = await getWorker()
 
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
     async initalize() {
         return Controller.initialize({
-            view: new View({}),
-            service: new Service({})
-
+            view: new View(),
+            service: new Service({}),
+            worker
         })
     }
 }
